9290d50c0ef7844b902949143677604b98867790
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-01 17:43:37 +0300

    RAndom sum Example 003

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.Write("первое случайное число ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("второе случайное число ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("результат сложения ");
 Console.WriteLine(result);
