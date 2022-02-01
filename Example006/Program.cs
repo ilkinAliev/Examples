c399b334486e9941523ed2e12c277440ed680f4b
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-01 20:22:41 +0300

    Example 006

---
+++ Example006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int c = 4;
+int d = 7;
+int e = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
