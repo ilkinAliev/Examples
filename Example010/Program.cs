821c3f3be68e60eea6b8e3feb02d5846c9680315
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 20:48:31 +0300

    Example 010 Array and while

---
+++ Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 33, 45, 65, 78, 76, 344, 32, 12, 344 };
+
+int n = array.Length;
+int find = 344;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
